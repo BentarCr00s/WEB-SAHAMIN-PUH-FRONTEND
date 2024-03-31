fed4038793472b222e26d327cc53ed0271003f2b
D. RikaZ <dzakirsolihin@gmail.com>
2024-04-01 05:30:54 +0700

    final membuat kalkulator page

---
+++ Scripts/navbarTemplate.js
@@ -0,0 +1,8 @@
+// ================ RESPONSIVE NAVBAR ================ //
+const menuButton = document.querySelector(".nav-list-button");
+const menuList = document.querySelector(".menu-area");
+
+menuButton.addEventListener("click", () => {
+    menuList.classList.toggle("active");
+});
+// ================ RESPONSIVE NAVBAR ================ //
