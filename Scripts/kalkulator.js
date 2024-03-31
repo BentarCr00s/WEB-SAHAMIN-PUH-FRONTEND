83eb9ac13fd19ae37a43b13953812e509a311ae1
Bentar Ristianto Sanjaya <49648965+BentarCr00s@users.noreply.github.com>
2024-04-01 01:53:54 +0700

    Saham & Kalkulator

---
+++ Scripts/kalkulator.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("#result").click(function () {
+    var buy = parseFloat($("#buy").val());
+    var buyCommission = parseFloat($("#buyCommission").val());
+    var sell = parseFloat($("#sell").val());
+    var lot = parseFloat($("#lot").val());
+    var sellCommission = parseFloat($("#sellCommission").val());
+    var totBuy = buy * lot + buy * lot * (buyCommission / 100);
+    var totSell = sell * lot - sell * lot * (sellCommission / 100);
+    var profitLoss = totSell - totBuy;
+    var profitLossPercent = (profitLoss / totBuy) * 100;
+    $("#totBuy").text(totBuy.toFixed(2));
+    $("#totSell").text(totSell.toFixed(2));
+    $("#profitLoss").text(profitLoss.toFixed(2));
+    $("#profitLossPercent").text(profitLossPercent.toFixed(2) + "%");
+  });
+});
